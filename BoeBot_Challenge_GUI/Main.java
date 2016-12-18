0a7e087ee7ea92e9a59c8b2a396d04de5daa1809
ArthurvanStrien <arthurvanstrien@gmail.com>
2016-12-18 21:01:34 +0100

    created a layout with buttons

--- BoeBot_Challenge_GUI/Main.java
+++ BoeBot_Challenge_GUI/Main.java
@@ -1,4 +1,6 @@
-
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
 /**
  * Write a description of class Main here.
  * 
@@ -6,10 +8,38 @@
  * @version (1)
  * @date    ()
  */
-public class Main
+public class Main extends JFrame
 {
-     public static void main(String[] args)
-     {
-         
-     }
+    JPanel panel;
+    JButton goButton;
+    JButton stopButton;
+    
+    public Main()
+    {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        panel = new JPanel(new BorderLayout());
+        setContentPane(panel);
+        
+        panel.add(createControlButtons(), BorderLayout.CENTER);
+        
+        setSize(600, 300);
+        setVisible(true);
+    }
+    
+    private JPanel createControlButtons()
+    {
+        JPanel buttons = new JPanel(new GridLayout(1, 2));
+        
+        ImageIcon goImage = new ImageIcon("go.png");
+        goButton = new JButton(goImage);
+        goButton.setBackground(Color.WHITE);
+        buttons.add(goButton);
+        
+        ImageIcon stopImage = new ImageIcon("stopSmall.jpg");
+        stopButton = new JButton(stopImage);
+        stopButton.setBackground(Color.WHITE);
+        buttons.add(stopButton);
+        
+        return buttons;
+    }
 }
