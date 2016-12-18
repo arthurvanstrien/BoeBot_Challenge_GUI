1b1d6d1a4cb61fd9a4ca95d7522efb6f2d6f1205
ArthurvanStrien <arthurvanstrien@gmail.com>
2016-12-18 20:36:31 +0100

    added project files and a class main.

---
+++ BoeBot_Challenge_GUI/Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author  (Arthur van Strien) 
+ * @version (1)
+ * @date    ()
+ */
+public class Main
+{
+     public static void main(String[] args)
+     {
+         
+     }
+}
