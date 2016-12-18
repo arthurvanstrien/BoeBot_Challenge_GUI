0a5755ce304ba3ccf734a13b4902f581b819c0d0
ArthurvanStrien <arthurvanstrien@gmail.com>
2016-12-18 21:02:46 +0100

    created a control class wihere the actions of the buttons can be added.

---
+++ BoeBot_Challenge_GUI/Control.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+/**
+ * Write a description of class Main here.
+ * 
+ * @author  (Arthur van Strien) 
+ * @version (1)
+ * @date    ()
+ */
+public class Control extends Main
+{
+    public Control()
+    {
+        goButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+        
+        stopButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+               
+            }
+        });
+    }
+}
